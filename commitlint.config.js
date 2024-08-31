2156fe8cfc97887d43a8dba0eaa6001ffd3932ac
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-08-31 13:05:35 -0500

    feat(lang): add Polish language

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,26 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'style',
+        'test',
+        'revert',
+      ],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0],
+  },
+};
