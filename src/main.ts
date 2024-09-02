afef22b79c959923bf7d06e748410885d3e94a8f
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-09-02 06:22:59 -0500

    build: cors added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Logger as logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(process.env.PORT || 3400);
   logger.log(`App running on port ${process.env.PORT}`);
 }
---
+++ src/modules/auth/services/login/index.ts
@@ -0,0 +1,4 @@
+export * from './check-password.service';
+export * from './check-user-exist.service';
+export * from './create-token.service';
+export * from './login.service.service';
