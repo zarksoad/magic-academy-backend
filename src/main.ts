9be070a0a69b885d4b055de00d1cd19902dd6e04
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-09-02 06:28:42 -0500

    feat: added global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger as logger } from '@nestjs/common';
+import { Logger as logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strips out properties not specified in the DTO
+      forbidNonWhitelisted: true, // Throws an error if any unspecified properties are present
+      transform: true, // Automatically transforms input data to the expected types
+    }),
+  );
   await app.listen(process.env.PORT || 3400);
   logger.log(`App running on port ${process.env.PORT}`);
 }
