ffa9fd4e6a33e41b249516e54ebaaec180695053
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-09-23 11:30:56 -0500

    refactor: refactor get courses by instructor id

--- src/modules/course/services/find-course-by-user.service.ts
+++ src/modules/course/services/find-course-by-user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Course } from '../entities/course.entity';
+import { CourseDto } from '../dto/dto-output/findAllCourse.dto';
 
 @Injectable()
 export class FindCoursesByUserService {
@@ -10,13 +11,25 @@ export class FindCoursesByUserService {
     private readonly courseRepository: Repository<Course>,
   ) {}
 
-  async findCoursesByUserId(userId: number): Promise<Course[]> {
+  async findCoursesByUserId(userId: number): Promise<CourseDto[]> {
     const courses = await this.courseRepository
       .createQueryBuilder('course')
-      .innerJoin('course.users', 'user')
+      .leftJoinAndSelect('course.users', 'user') 
       .where('user.id = :userId', { userId })
       .getMany();
 
-    return courses;
+    console.log(courses);
+
+    const transformedResponse = courses.map(course => ({
+      id: course.id,
+      name: course.name,
+      description: course.description,
+      thumbnail_url: course.thumbnail_url,
+      slug: course.slug,
+      published_at: course.published_at,
+      instructor_name: course.users?.[0]?.name || 'Unknown',
+    }));
+
+    return transformedResponse;
   }
 }
